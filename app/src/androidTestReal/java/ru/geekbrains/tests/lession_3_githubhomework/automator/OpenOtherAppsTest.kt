fcb59137903783ff8782c4fd15ffaf1984aa0db2
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-03-28 15:13:58 +0300

    Выполнен 3 пункт ДЗ для самостоятельного изучения: Средствами UiAutomator запущено приложение "Scientific Calculator" и нажата кнопка "1"
    Средствами UiAutomator запущено приложение "Scientifit Calculator" на телефоне "Pixel 4" с API 30 и в нём нажата кнопка "1".
    
    Адрес репозитория:
    https://github.com/AVKhakhalin/Lession_3_TestGitHubHomeWork

--- app/src/androidTestReal/java/ru/geekbrains/tests/lession_3_githubhomework/automator/OpenOtherAppsTest.kt
+++ app/src/androidTestReal/java/ru/geekbrains/tests/lession_3_githubhomework/automator/OpenOtherAppsTest.kt
@@ -4,9 +4,7 @@ import android.widget.TextView
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiScrollable
-import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.*
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -39,5 +37,27 @@ class OpenOtherAppsTest {
         val settingsValidation =
             uiDevice.findObject(UiSelector().packageName("com.android.settings"))
         Assert.assertTrue(settingsValidation.exists())
+    }
+
+    @Test // Открытие приложение ScientificCalculator
+    fun test_OpenScientificCalculator() {
+        // Нажимаем на системную кнопку Home()
+        uiDevice.pressHome()
+        // Реализация смахивания для открытия нижнего системного меню с приложениями
+        uiDevice.swipe(500, 1500, 500, 0, 5)
+        // Установка скроллинга приложений
+        val appViews = UiScrollable(UiSelector().scrollable(true))
+        // Находим в контейнере настройки по названию иконки "Settings"
+        val settingsApp = appViews
+            .getChildByText(
+                UiSelector()
+                    .className(TextView::class.java.name),
+                "Scientific calculator") // Название хранится в манифесте в поле android:label
+        // Открываем приложение "Настройки"
+        settingsApp.clickAndWaitForNewWindow()
+        // Находим кнопку 1
+        val buttonOne: UiObject = uiDevice.findObject(UiSelector().text("1"))
+        //Кликаем по кнопке 1
+        buttonOne.click()
     }
 }
